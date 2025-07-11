16d04efc93acdec90e7396ab4922f59ee658e696
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-07-11 16:57:50 +0530

    api and error handlers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv';
 import connectDb from './db/index.js';
+import app from './app.js';
 
 dotenv.config({ path: './.env' });
 
-connectDb();
+connectDb()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running on Port: ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection Failed", err);
+  throw err;
+});
 
 /*
 import mongoose from "mongoose";
